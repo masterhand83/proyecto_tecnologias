c422d342cafcdeb6b5dfec1dd4f011b4000e4a58
masterhand83 <robinmiguelsdj@gmail.com>
2021-11-08 00:48:37 -0600

    agregando conexion menu-ejercicio

--- react/app/components/Ejercicio.jsx
+++ react/app/components/Ejercicio.jsx
@@ -3,10 +3,15 @@ import  React  from "react";
 export default class Ejercicio extends React.Component {
     constructor(props){
         super(props)
+        console.log(props)
+        const query = new URLSearchParams(this.props.location.search)
+        this.state = {
+            ejercicioID: query.get('id')
+        }
     }
     render(){
         return(
-            <h3>EfjñakljfasñklfjñlJÑFHAÑWFIOHÑAFIOHAEÑOIFH</h3>
+            <h3>{this.state.ejercicioID}</h3>
         )
     }
 }
